08dacbe4c5b407b19f672a6d3f27627c68694228
bogdan <bohdan549@gmail.com>
2016-10-11 16:51:48 +0300

    third_commit

--- CircularPrimes.cs
+++ CircularPrimes.cs
@@ -7,77 +7,38 @@ using System.Threading;
 
 namespace CircularPrimes
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int N = 10000;
-            int index = 0;
-            List<double> prime = new List<double>();
-            List<double> circularPrime = new List<double>();
-            char spare,spare1;
-            bool flag;
-            for (int i = 2; i <= N; i++)
-                prime.Add(i);
-            for (int i = 0; i < prime.Count; i++)
-            { 
-               for(int k = i+1 ; k < prime.Count; k++)
-                   if (prime[k] % prime[i] == 0)
-                       {
-                           prime[k] = 0;            
-                       }
-            }            
-            for (int i = 0; i < prime.Count; i++)
-                {
-                    if (prime[i] == 0)
-                        {
-                            for (int k = i + 1; k < prime.Count; k++)
-                                {
-                                    if (prime[k] != 0)
-                                    {
-                                        index = k;
-                                        break;  
-                                    }
-                                }
-                            prime[i] = prime[index];
-                            prime[index] = 0; 
-                        }
-                }
-            prime.RemoveAll(x => x == 0);
-            for (int i = 0; i < prime.Count; i++)
+    class PrimeNumbers {
+        List<int> primes;        
+        public List<int> Primes{          
+          get
             {
-                flag = true;
-                var mas = prime[i].ToString().ToCharArray();
-                if (mas.Length == 1)
+               return this.primes;          
+            }
+        }
+        public PrimeNumbers()
+        {
+            this.primes = new List<int>();            
+         }
+        public List<int> findPrimes(int size)
+        {
+            this.primes.Add(0);
+            this.primes.Add(0);
+            for (int i = 2; i <= size; i++)
+                this.primes.Add(i);
+            for (int i = 2; i * i < this.primes.Count; i++)
+            {
+                if (this.primes[i] != 0)
                 {
-                    // var sth =  Convert.ToString(mas);
-                    circularPrime.Add(Convert.ToDouble(new string(mas)));
-                }
-                else
-                {
-                    for (int j = 0; j < mas.Length - 1; j++)
+                    for (int j = i * this.primes[i]; j < this.primes.Count; j += this.primes[i])
                     {
-                        for (int k = 0; k < mas.Length - 1; k++)
-                        {
-                            spare = mas[k];
-                            spare1 = mas[k + 1];
-                            mas[k] = spare1;
-                            mas[k + 1] = spare;
-                        }
-                        double number = Convert.ToDouble(new string(mas));
-                        if (ISprime(number))
-                            flag = false;
+                        this.primes[j] = 0;
                     }
-                    if (flag == true)
-                        circularPrime.Add(prime[i]);
                 }
             }
-            //var mas = prime[5].ToString().ToCharArray();
-            foreach (var i in circularPrime)
-                Console.WriteLine(i);
-            Thread.Sleep(90000);
+            this.primes.RemoveAll(x => x == 0);
+            return this.Primes;
         }
-        static bool ISprime(double number)
+        public bool ISprime(int number)
         {
             if (number == 1)
                 return false;
@@ -89,7 +50,70 @@ namespace CircularPrimes
                     break;
                 }
             }
-            return false;
+            return false;       
         }
+        public void show()
+        {
+            foreach (var i in this.primes)
+                Console.WriteLine(i);
+        }
+    }
+    class CircularPrimes : PrimeNumbers {
+        List<int> circularPrimes;             
+        public List<int> circularprimes {
+            get
+            {
+                return this.circularPrimes;
+            }
+        }
+        public CircularPrimes() : base() { 
+             this.circularPrimes = new List<int>();                           
+        }
+        public List<int> findCirculars(int size){
+            bool flag;
+            char spare, spare1;
+            List<int> primes = base.findPrimes(size).GetRange(0,base.Primes.Count);
+            for (int i = 0; i < primes.Count; i++)
+            {
+                flag = true;
+                var mas = primes[i].ToString().ToCharArray();
+                if (mas.Length == 1)
+                {
+                    this.circularPrimes.Add(Convert.ToInt32(new string(mas)));
+                }
+                else
+                {
+                    for (int j = 0; j < mas.Length - 1; j++)
+                    {
+                        for (int k = 0; k < mas.Length - 1; k++)
+                        {
+                            spare = mas[k];
+                            spare1 = mas[k + 1];
+                            mas[k] = spare1;
+                            mas[k + 1] = spare;                           
+                        }
+                        int number = Convert.ToInt32(new string(mas));
+                        if (base.ISprime(number))
+                            flag = false;
+                    }
+                    if (flag == true)
+                        this.circularPrimes.Add(primes[i]);
+                }
+            }
+            return this.circularPrimes;  
+        }
+        public void show() {                          
+            foreach (var i in this.circularPrimes)
+                    Console.WriteLine(i);            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CircularPrimes cl = new CircularPrimes();            
+            Console.WriteLine(cl.findCirculars(1000000).Count);            
+            Console.ReadKey();            
+        }      
     }
 }
--- SumOfelements.cs
+++ SumOfelements.cs
@@ -5,54 +5,62 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Threading;
+using System.Windows.Forms;
 namespace Tsk1
 {
+    class Sum {
+        List<List<double>> mas;
+        StreamReader reader;
+        public Sum()
+        {
+            using (OpenFileDialog file = new OpenFileDialog())
+            {
+                int counter, N = 0;
+                this.mas = new List<List<double>>();
+                file.ShowDialog();
+                this.reader = new StreamReader(@"" + (file.FileName).Replace("/", "\\"));
+                while (reader.ReadLine() != null)
+                {
+                    N += 1;
+                }
+                this.reader.BaseStream.Position = 0;
+                for (int i = 0; i < N; i++)
+                {
+                    this.mas.Add(new List<double>());
+                    for (int j = 0; j < N; j++)
+                    {
+                        this.mas[i].Add(0);
+                    }
+                    counter = 0;
+                    foreach (var elem in reader.ReadLine().Split(' '))
+                    {
+                        this.mas[i].RemoveAt(counter);
+                        this.mas[i].Insert(counter, Convert.ToDouble(elem));
+                        counter += 1;
+                    }
+                }
+            }
+        }       
+     public double TriangleSum() {
+            double sum = 0, number;
+            for (int i = 0; i < mas.Count; i++)
+                { 
+                   number = this.mas[i][0];
+                   this.mas[i].ForEach((x) => { if (x > number) number = x; });
+                   sum += number;
+                }
+            return sum;
+        }
+
+    }
     class Program
     {
         [STAThread]
         static void Main(string[] args)
         {
-            List<List<double>> mas;
-            StreamReader reader;
-            int counter,N = 0;          
-            using (System.Windows.Forms.OpenFileDialog file = new System.Windows.Forms.OpenFileDialog())
-            {
-                mas = new List<List<double>>();
-                file.ShowDialog();                
-                reader = new StreamReader(@"" + (file.FileName).Replace("/", "\\"));
-                while (reader.ReadLine()!=null)
-                    {
-                        N += 1;                   
-                    }
-                reader.BaseStream.Position = 0;
-                for (int i = 0; i < N; i++)
-                {
-                    mas.Add(new List<double>());
-                    for (int j = 0; j < N; j++)
-                    {
-                        mas[i].Add(0);
-                    }
-                    counter = 0;
-                    foreach (var elem in reader.ReadLine().Split(' '))
-                    {
-                        mas[i].RemoveAt(counter);
-                        mas[i].Insert(counter,Convert.ToDouble(elem));
-                        counter+=1;
-                    }  
-                 }               
-                Console.WriteLine("The sum is "+TriangleSum(mas));
-                Console.ReadKey();
+            Sum cl = new Sum();
+            Console.WriteLine("The sum is "+cl.TriangleSum());
+            Console.ReadKey();
             }
-        }
-     private static double TriangleSum(List<List<double>> mas ) {
-            double sum = 0, number;
-            for (int i = 0; i < mas.Count; i++)
-                { 
-                   number = mas[i][0];
-                   mas[i].ForEach((x) => { if (x > number) number = x; });
-                   sum += number;
-                }
-            return sum;
-        }
-    }
+        }     
 }
